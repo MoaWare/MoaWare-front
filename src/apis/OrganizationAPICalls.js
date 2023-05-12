247bf1c956a2f05353b04a61c43e643b9ef3bd49
cloverpig <byoungoh.kang@gmail.com>
2023-05-12 19:00:25 +0900

    2023.05.12 feat 조직도 검색 api 완료

--- src/apis/OrganizationAPICalls.js
+++ src/apis/OrganizationAPICalls.js
@@ -4,6 +4,7 @@ const RESTAPI_SERVER_IP = `${ process.env.REACT_APP_RESTAPI_SERVER_IP}`;
 const RESTAPI_SERVER_PORT = `${ process.env.REACT_APP_RESTAPI_SERVER_PORT}`
 const PRE_URL = `http://${RESTAPI_SERVER_IP}:${RESTAPI_SERVER_PORT}/org`
 
+/* 조직도 전체 검색 */
 export const CallOrganizationListAPI = () => {
 
     const requestURL = `${ PRE_URL}/list`;
@@ -17,6 +18,26 @@ export const CallOrganizationListAPI = () => {
         console.log( result);
         if ( result.status === 200 ) {
             console.log ("[OrganizationAPICalls] CallOrganizationListAPI result : ", result);
+            dispatch(getOrganization(result));
+        }
+    };
+
+}
+
+/* 조직도 검색  */
+export const CallOrganizationSearchAPI = ({ search }) => {
+
+    const requestURL = `${ PRE_URL}/search?search=${ search }`;
+
+    return async( dispatch, getState ) => {
+        
+        const result = await fetch( requestURL, {
+            method: 'GET'
+        }).then( res => res.json());
+
+        console.log( result);
+        if ( result.status === 200 ) {
+            console.log ("[OrganizationAPICalls] CallOrganizationSearchAPI result : ", result);
             dispatch(getOrganization(result));
         }
     };
