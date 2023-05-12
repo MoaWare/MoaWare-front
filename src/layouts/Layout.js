7fcbae916f76803774fdb25742250d53275b5c64
cloverpig <byoungoh.kang@gmail.com>
2023-05-12 11:03:51 +0900

    2023.05.12 feat 배경색 및 조직도 컴포넌트 추가

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,9 +1,11 @@
+import { Outlet } from 'react-router';
 import Header from '../components/common/Header';
 
 function Layout() {
   return (
     <>
         <Header />
+        <Outlet/>
     </>
   );
 }
