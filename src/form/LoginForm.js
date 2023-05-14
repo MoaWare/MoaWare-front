5cbae7106d364ce0fb6eba7df36011cfd083f726
haeunyy <ibbihe@gmail.com>
2023-05-14 18:42:31 +0900

    docs : 로그인 화면 구성 정리 및 css 추가

--- src/form/LoginForm.js
+++ src/form/LoginForm.js
@@ -1,9 +1,12 @@
 import { useDispatch } from "react-redux";
 import { callLoginAPI } from "../apis/EmployeeAPICalls";
 import { useState } from "react";
+import LoginCSS from "./Login.module.css"
+import { useNavigate } from "react-router-dom";
 
 function Loginform(){
 
+    const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const [form,setForm] = useState({
@@ -22,8 +25,18 @@ function Loginform(){
         dispatch(callLoginAPI(form));
     }
 
+    const onClickIdHandler = () => {
+        navigate('/idfind');
+    }
+
+    const onClickPwdHandler = () => {
+        navigate('/pwdfind');
+    }
+
     return (
         <>
+            <h1>Login</h1><br/>
+            <span>모아웨어에 오신 것을 환영합니다.</span>
             <input 
                 type="text" 
                 placeholder="아이디"
@@ -37,6 +50,9 @@ function Loginform(){
                 onChange={onChangeHandler}
                 /><br/>
             <button onClick={onClickHandler} className="login-button">LOGIN</button><br/>
+            <div className={ LoginCSS.loginfind }>
+                <button onClick={onClickIdHandler}>아이디 찾기</button><span>|</span><button onClick={onClickPwdHandler}>비밀번호 찾기</button>
+            </div>
         </>
     )
 }
--- src/pages/member/Login.js
+++ src/pages/member/Login.js
@@ -25,30 +25,16 @@ function Login(){
         },[login]
     )
 
-    const onClickIdHandler = () => {
-        navigate('/idfind');
-    }
-
-    const onClickPwdHandler = () => {
-        navigate('/pwdfind');
-    }
 
     return (
         <div className={LoginCSS.backgroundDiv}>
             <div className={ LoginCSS.loginDiv }>
                 <img src="./icon/moawareLoginMain.png" alt='Down' name="dept"/><br/>
                 <img src="./icon/Line 1.png" alt='Down' name="dept" className={LoginCSS.line}/><br/>
-                <div className={LoginCSS.loginDivForm}>
-                <h1>Login</h1><br/>
-                <span>모아웨어에 오신 것을 환영합니다.</span>
                 <LoginForm />
-                <div className={ LoginCSS.loginfind }>
-                    <button onClick={onClickIdHandler}>아이디 찾기</button><span>|</span><button onClick={onClickPwdHandler}>비밀번호 찾기</button>
-                </div>
-                </div>
             </div>
         </div>
-    )
+    )     
 }
 
 export default Login;
