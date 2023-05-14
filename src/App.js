adb67cc3da0ecf4ab780ea8e891cf0cf7a904217
dldbaud <dldbaud20@gmail.com>
2023-05-15 02:07:07 +0900

    feat : 근태 현황, 연차 신청 목록 페이지 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import OrganizaionList from "./pages/organization/OrganizationList";
 import './App.css';
 import Work from "./pages/users/works/Work";
+import WorkLayout from './layouts/WorkLayout';
+import WorkRestReq from './pages/users/works/WorkRestReq';
+import WorkRestReqList from './pages/users/works/WorkRestReqList';
+import WorkRestList from './pages/users/works/WorkRestList';
 
 
 function App() {
@@ -17,9 +21,12 @@ function App() {
             <Route index element={<OrganizaionList/>}/>
           </Route>
           {/* 근태관리 탭 */}
-            <Route path="work">
-          {/* <Route path="work" element={ <WorkLayout/>}> */}
-            <Route index element={<Work/>}/> 
+            {/* <Route path="work"> */}
+          <Route path="work" element={ <WorkLayout/>}>
+            <Route index element={<Work/>}/>
+            <Route path="restReq" element={<WorkRestReq/>}/>
+            <Route path="restReqList" element={<WorkRestReqList/>}/>
+            <Route path="restList" element={<WorkRestList/>}/>
           </Route>
         </Route>
       </Routes>
