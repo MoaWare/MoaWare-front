98ff8ba3e4386c772b62bb17021561fb6ea6d429
전효진 <hj90886844@gmail.com>
2023-05-15 17:33:17 +0900

    효진 / 메인페이지 구현

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -15,7 +15,6 @@ const Header = ({ empName, onLogout }) => {
     navigate("/work");
   }
 
-
   return (
     <div className={HeaderCSS.header}>
       <div>
@@ -30,7 +29,7 @@ const Header = ({ empName, onLogout }) => {
           <li><Link to="/">일정관리</Link></li>
           <li><Link to="/">게시판</Link></li>
           {/* <li><Link to="/">근태관리</Link></li> */}
-           <li><button className={ HeaderCSS.btn}
+           <li><button className={HeaderCSS.btn}
            onClick={ onClickWorkHandler }
            >근태관리</button></li>
           <li><Link to="/">메신저</Link></li>
