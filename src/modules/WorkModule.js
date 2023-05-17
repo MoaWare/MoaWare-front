281e63479a85a237ccb4fa276739777f4f7361ca
dldbaud <dldbaud20@gmail.com>
2023-05-17 17:39:47 +0900

    유명 // 근태현황 페이징 바 고침

--- src/modules/WorkModule.js
+++ src/modules/WorkModule.js
@@ -5,14 +5,17 @@ const initalState = [];
 
 /* 액션 */
 const GET_WORKLIST = 'work/GET_WORKLIST';
+const POST_INSERTSTART = 'work/POST_INSERTSTART';
 
-export const { work : { getWorklist }} = createActions({
-    [GET_WORKLIST] : (res) => res.data
+export const { work : { getWorklist,postInsertstart }} = createActions({
+    [GET_WORKLIST] : (res) => res.data,
+    [POST_INSERTSTART] : res => res
 });
 
 const workReducer = handleActions(
     {
-        [GET_WORKLIST] : (state, { payload }) => ({ myWork : payload })
+        [GET_WORKLIST] : (state, { payload }) => ({ myWork : payload }),
+        [POST_INSERTSTART] : (state, { payload }) => ({ insert : payload})
 
     }
 , initalState)
