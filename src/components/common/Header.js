6da9a589eda5d34d02a2dcaadd8089b201fe2371
HYUNJOONSHIN <casillaslux@gmail.com>
2023-05-18 17:22:37 +0900

    test 게시판 crud 프론트 구현 중

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,21 +8,21 @@ const Header = ({ empName, onLogout }) => {
 
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    
-  },[])
+  useEffect(() => {
+
+  }, [])
 
   const onClickLogoutHandler = () => {
-    
-    if(window.confirm('로그아웃 하시겠습니까?')){
+
+    if (window.confirm('로그아웃 하시겠습니까?')) {
       window.localStorage.removeItem('accessToken');
       console.log('로그아웃 확인');
       navigate('/');
       console.log('로그아웃 확인');
-    } 
-  } 
+    }
+  }
 
-  
+
 
   return (
     <div className={HeaderCSS.header}>
@@ -36,7 +36,7 @@ const Header = ({ empName, onLogout }) => {
           <li><Link to="/pay">전자결재</Link></li>
           <li><Link to="/">시설예약</Link></li>
           <li><Link to="/">일정관리</Link></li>
-          <li><Link to="/">게시판</Link></li>
+          <li><Link to="/board">게시판</Link></li>
           <li><Link to="/work">근태관리</Link></li>
           <li><Link to="/">메신저</Link></li>
           <li><Link to="/org">조직도</Link></li>
