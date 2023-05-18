6f61c4246a0d1a03d39f323ac2087027fe4d702d
HYUNJOONSHIN <casillaslux@gmail.com>
2023-05-18 09:49:19 +0900

    test. 게시판 crud 기능 구현중(프론트)

---
+++ src/pages/board/BoardPostList.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { callBoardPostListAPI } from "../../../src/apis/BoardPostAPICall";
+import { useParams, useSearchParams } from "react-router-dom";
+import BoardPostListCSS from ".BoardPostList.module.css";
+function BoardPost() {
+
+    const dispatch = useDispatch();
+
+
+
+    return (
+        <>
+
+
+
+
+
+
+
+
+
+        </>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    );
+
+
+
+}
