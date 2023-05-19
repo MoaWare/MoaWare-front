98d1ed193741ef1867e56a083be1c116fcd9180d
hyojin jeon <hj90886844@gmail.com>
2023-05-19 17:24:10 +0900

    효진 / 일정관리 구글계정 연결

--- src/pages/schedule/Schedule.js
+++ src/pages/schedule/Schedule.js
@@ -10,12 +10,29 @@ function Schedule() {
     return (
         <div className='wrapper'>
             <div className='wrap'>
-                <FullCalendar 
+                <FullCalendar
                     plugins={[dayGridPlugin, googleCalendarPlugin]}
                     googleCalendarApiKey={'AIzaSyD6amFkYvgsMyc38D6Sq8zTKs7gCWrwtE0'}
                     events={{
-                        googleCalendarId: 'moaware@gmail.com',
+                        googleCalendarId: 'moawarew@gmail.com',
                     }}
+                    headerToolbar={{
+                        left: 'prev,today,next',
+                        center: 'title',
+                        right: 'dayGridYear,dayGridMonth,dayGridWeek,dayGridDay',
+                    }}
+                    buttonText={{
+                        year: '년간',
+                        month: '월간',
+                        week: '주간',
+                        day: '일간',
+                        today: '오늘'
+                    }}
+                    locale='ko'
+                    // 타이틀 수정하는 코드 https://fullcalendar.io/docs/titleFormat
+                    // titleFormat={{
+                    //     year: 'numeric', month: 'numeric', day:'2-digit'
+                    // }}
                 />
             </div>
         </div>
