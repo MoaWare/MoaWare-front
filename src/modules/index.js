a1963a28f17a590855bda34651736ec981802367
cloverpig <byoungoh.kang@gmail.com>
2023-05-19 17:39:37 +0900

    2023.05.19 병오 : feat 양식 저장 코드 완성

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,10 +4,11 @@ import organizationReducer from "./OrganizationModule";
 import employeeReducer from "./EmployeeModule";
 import workReducer from "./WorkModule";
 import workTimeReducer from "./WorkTimeModule";
+import paymentReducer from "./PayMentModule";
 
 
 const rootReducer = combineReducers({
-    boardReducer, organizationReducer, employeeReducer, workReducer ,workTimeReducer
+    boardReducer, organizationReducer, employeeReducer, workReducer ,workTimeReducer, paymentReducer
 });
 
 export default rootReducer
--- src/pages/payment/PaymentList.js
+++ src/pages/payment/PaymentList.js
@@ -1,5 +1,7 @@
 import { useEffect,useState } from 'react';
 import payCSS from './Payment.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { CallPaymentListAPI } from '../../apis/PaymentAPICalls';
 
 
 
@@ -8,8 +10,43 @@ function PaymentList () {
     const [name, setName] = useState('');
     const [name2, setName2] = useState('');
     const [number, setNumber] = useState('');
+    const [dept, setDept] = useState('');
+    const [job, setJob] = useState('');
+    const [payStatus, setPayStatus] = useState('');
+    const [payDate, setPayDate] = useState('');
+    const [openTime, setOpenTime] = useState('');
+    const [closeTime, setCloseTime] = useState('');
+    const [text, setText] = useState('');
     const [htmlString, setHtmlString] = useState('');
+    const dispatch = useDispatch();
+    const { pay } = useSelector( state => state.paymentReducer );
+
+    const [ form, setForm ] = useState({});
+
+    const onChangeHandler = (e) => {
+      setForm(
+        {
+          ...form,
+          [e.target.name] : e.target.value
+        }
+      );
+
+      console.log("form 핸들러 : " , form);
+
+    }
+
+    const [ isButton, setIsButton ] = useState(false);
+    const onButtonHandler= () => {
+      if(isButton){
+        setIsButton(false);
+      } else {
+        setIsButton(true)
+      }
+    }
+
+    const payPath = pay&&pay[0].form.formPath;
   
+    // console.log( "payPath : ", payPath);
     const handleNameChange = (event) => {
       setName(event.target.value);
     };
@@ -20,130 +57,102 @@ function PaymentList () {
     const handleNumberChange = (event) => {
       setNumber(event.target.value);
     };
-  
-    const saveHtmlString = () => {
-      const html = `
-        <div>
-        <label class=${payCSS.payName}>이름</label>
-        <span  class=${payCSS.input} >${name}</span>
-        </div>
-        <div>
-        <label>번호</label>
-        <input type="text" value="${number}" readonly />
-        <label>번호1</label>
-        <input type="text" value="${number}" disabled />
-      </div>
 
-      <table className={payCSS.docuDiv}>
-                      <tbody className={payCSS.docuDiv}>
-                        <tr>
-                          <th colSpan='4' className={payCSS.docuTitle}>출퇴근 사유서</th>
-                        </tr>
-                        <tr>
-                          <th className={payCSS.docuLabel}>성 명</th>
-                          <td><input type='text' className={payCSS.docuInput}
-                          value={name2} onChange={handleNameChange2}/></td>
-                          <th>부 서</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                        </tr>
-                        <tr>
-                          <th>직 급</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                          <th>처리 구분</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                        </tr>
-                        <tr>
-                          <th>일시</th>
-                          <td colSpan='3'><input type='text' className={payCSS.docuInput}/></td>
-                        </tr>
-                        <tr>
-                          <th>출근 시간</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                          <th>퇴근 시간</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                        </tr>
-                        <tr>
-                          <th rowSpan="2">사유</th>
-                          <td rowSpan="2" colSpan="3"><input type='text' className={payCSS.docuInputText}/></td>
-                        </tr>
-                      </tbody>
-                    </table>
-    `;
-      setHtmlString(html);
-      // 변환된 HTML 문자열을 서버에 저장하는 로직을 추가해야 합니다.
+    const handledeptChange = (event) => {
+      setDept(event.target.value);
     };
 
-    const addReadonlyToInput = (htmlString) => {
-        const modifiedHTML = htmlString.replace(/<input[^>]*>/g, (match) => {
-          return match.replace(/className=/, 'readOnly class=$').replace(/value={([^}]*)}/, 'value=${$1}')
-        });
+    
+  
+    
+    useEffect(() => {
+      
+      dispatch(CallPaymentListAPI());
+
+      const processHtmlString = (html) => {
+
+        let modifiedHTML = html;
+        Object.entries(form).forEach(([key, value]) => {
+          const regex = new RegExp(`{${key}}`, "g");
+
+          if (form[key] === undefined || form[key] === null) {
+            modifiedHTML = modifiedHTML.replace(regex, "");
+          } else {
+            modifiedHTML = modifiedHTML.replace(regex, `"${value}"`.trim() || " ");
+          }
+       
+        });  
+   
         return modifiedHTML;
       };
       
-      const filteredHTML = addReadonlyToInput(htmlString);
+      console.log("form : ", form);
+      // DB에서 가져온 HTML 문자열
+      const htmlFromDB = payPath;
+      if (payPath) {
+      const filteredHTML = processHtmlString(htmlFromDB);
+      console.log( "filteredHTML : ", filteredHTML);
+      setHtmlString(filteredHTML);
+      }
+    }, [isButton]);
 
-      useEffect(() => {
-        const addReadonlyToInput = (htmlString) => {
-          const modifiedHTML = htmlString.replace(/<input[^>]*>/g, (match) => {
-            return match.replace(/className=/, 'readOnly class=$').replace(/value={([^}]*)}/, 'value={$1}');
-          });
-          return modifiedHTML;
-        };
-    
-        const filteredHTML = addReadonlyToInput(htmlString);
-        setHtmlString(filteredHTML);
-      }, [htmlString]);
-    
+      
+
     console.log("htmlString : ", htmlString);
-    console.log("filteredHTML : ", filteredHTML);
+    // console.log("filteredHTML : ", filteredHTML);
     return (
       <div className={payCSS.background}>
         <div>
           <label className={payCSS.payName} >이름</label>
-          <input type="text" value={name} onChange={handleNameChange} className={payCSS.input} />
+          <input type="text" value={name2} onChange={handleNameChange2} className={payCSS.input} />
         </div>
         <div>
           <label>번호</label>
           <input type="text" value={number} onChange={handleNumberChange} />.
         </div>
-        <button onClick={saveHtmlString}>HTML 저장하기</button>
-        <div dangerouslySetInnerHTML={{ __html: filteredHTML }} 
-        contentEditable={false}/>
-
+        <button onClick={onButtonHandler}>HTML 보여주기</button>
+        <div dangerouslySetInnerHTML={{ __html: `${htmlString}` }} />
               <table className={payCSS.docuDiv}>
-                      <tbody className={payCSS.docuDiv}>
-                        <tr>
-                          <th colSpan='4' className={payCSS.docuTitle}>출퇴근 사유서</th>
-                        </tr>
-                        <tr>
-                          <th className={payCSS.docuLabel}>성 명</th>
-                          <td><input type='text' className={payCSS.docuInput}
-                          value={name2} onChange={handleNameChange2}/></td>
-                          <th>부 서</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                        </tr>
-                        <tr>
-                          <th>직 급</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                          <th>처리 구분</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                        </tr>
-                        <tr>
-                          <th>일시</th>
-                          <td colSpan='3'><input type='text' className={payCSS.docuInput}/></td>
-                        </tr>
-                        <tr>
-                          <th>출근 시간</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                          <th>퇴근 시간</th>
-                          <td><input type='text' className={payCSS.docuInput}/></td>
-                        </tr>
-                        <tr>
-                          <th rowSpan="2">사유</th>
-                          <td rowSpan="2" colSpan="3"><input type='text' className={payCSS.docuInputText}/></td>
-                        </tr>
-                      </tbody>
-                    </table>
+                <tbody className={payCSS.docuDiv}>
+                  <tr>
+                    <th colSpan='4' className={payCSS.docuTitle}>출퇴근 사유서</th>
+                  </tr>
+                  <tr>
+                    <th className={payCSS.docuLabel}>성 명</th>
+                    <td><input type='text' name="name" className={payCSS.docuInput}
+                      value={form.name} onChange={onChangeHandler}/></td>
+                    <th>부 서</th>
+                    <td><input type='text' className={payCSS.docuInput} name="dept"
+                    value={form.dept} onChange={onChangeHandler}/></td>
+                  </tr>
+                  <tr>
+                    <th>직 급</th>
+                    <td><input type='text' className={payCSS.docuInput}
+                    name="job" value={form.job} onChange={onChangeHandler}/></td>
+                    <th>처리 구분</th>
+                    <td><input type='text' className={payCSS.docuInput}
+                    name="payStatus" value={form.payStatus} onChange={onChangeHandler}/></td>
+                  </tr>
+                  <tr>
+                    <th>일시</th>
+                    <td colSpan='3'><input type='text' className={payCSS.docuInput}
+                    name="payDate" value={form.payDate} onChange={onChangeHandler}/></td>
+                  </tr>
+                  <tr>
+                    <th>출근 시간</th>
+                    <td><input type='text' className={payCSS.docuInput}
+                    name="openTime" value={form.openTime} onChange={onChangeHandler}/></td>
+                    <th>퇴근 시간</th>
+                    <td><input type='text' className={payCSS.docuInput}
+                    name="closeTime" value={form.closeTime} onChange={onChangeHandler}/></td>
+                  </tr>
+                  <tr>
+                    <th rowSpan="2">사유</th>
+                    <td rowSpan="2" colSpan="3"><input type='text' className={payCSS.docuInputText}
+                    name="text" value={form.text} onChange={onChangeHandler}/></td>
+                  </tr>
+                </tbody>
+              </table>
       </div>
 
       
