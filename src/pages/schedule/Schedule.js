a5389ff8299130151f539848cecd8b0f4d37077d
hyojin jeon <hj90886844@gmail.com>
2023-05-19 09:20:15 +0900

    효진 / 일정관리 캘린더 구현

--- src/pages/schedule/Schedule.js
+++ src/pages/schedule/Schedule.js
@@ -1,28 +1,23 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+// import ScheduleCSS from './Schedule.module.css'
+import '../schedule/Schedule.css';
+
 function Schedule() {
 
     return (
-        <div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
-            <div>하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가하이룽가</div>
+        <div className='wrapper'>
+            <div className='wrap'>
+                <FullCalendar 
+                    plugins={[dayGridPlugin, googleCalendarPlugin]}
+                    googleCalendarApiKey={'AIzaSyD6amFkYvgsMyc38D6Sq8zTKs7gCWrwtE0'}
+                    events={{
+                        googleCalendarId: 'moaware@gmail.com',
+                    }}
+                />
+            </div>
         </div>
     );
 
