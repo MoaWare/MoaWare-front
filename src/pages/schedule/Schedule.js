376ff28b74bd724dd8548dff85937ad34e4fca28
hyojin jeon <hj90886844@gmail.com>
2023-05-24 09:12:02 +0900

    효진 / 풀 당기기전 커밋

--- src/pages/schedule/Schedule.js
+++ src/pages/schedule/Schedule.js
@@ -1,43 +1,51 @@
 import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
-// import ScheduleCSS from './Schedule.module.css'
+import interactionPlugin from '@fullcalendar/interaction'; // Import the interaction plugin
 import '../schedule/Schedule.css';
 
 function Schedule() {
+    
+  const handleEventDrop = (info) => {
+    // Handle event drop here
+    console.log('Event dropped:', info.event);
+  };
 
-    return (
-        <div className='wrapper'>
-            <div className='wrap'>
-                <FullCalendar
-                    plugins={[dayGridPlugin, googleCalendarPlugin]}
-                    googleCalendarApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-                    events={{
-                        googleCalendarId: 'moawarew@gmail.com',
-                    }}
-                    headerToolbar={{
-                        left: 'prev,today,next',
-                        center: 'title',
-                        right: 'dayGridYear,dayGridMonth,dayGridWeek,dayGridDay',
-                    }}
-                    buttonText={{
-                        year: '년간',
-                        month: '월간',
-                        week: '주간',
-                        day: '일간',
-                        today: '오늘'
-                    }}
-                    locale='ko'
-                    // 타이틀 수정하는 코드 https://fullcalendar.io/docs/titleFormat
-                    // titleFormat={{
-                    //     year: 'numeric', month: 'numeric', day:'2-digit'
-                    // }}
-                />
-            </div>
-        </div>
-    );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const schedule = useSelector(state => state.scheduleReducer);
 
+  return (
+    <div className='wrapper'>
+      <div className='wrap'>
+        <FullCalendar
+          plugins={[dayGridPlugin, googleCalendarPlugin, interactionPlugin]} // Include the interaction plugin
+          googleCalendarApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+          events={{
+            googleCalendarId: 'moawarew@gmail.com',
+          }}
+          headerToolbar={{
+            left: 'prev,today,next',
+            center: 'title',
+            right: 'dayGridYear,dayGridMonth,dayGridWeek,dayGridDay',
+          }}
+          buttonText={{
+            year: '연간',
+            month: '월간',
+            week: '주간',
+            day: '일간',
+            today: '오늘'
+          }}
+          locale='ko'
+          editable={true} // Enable event editing
+          eventDrop={handleEventDrop} // Specify the event drop callback
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Schedule;
