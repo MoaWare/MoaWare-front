e64621b0745187306223fed094ee1d44fd4b65c4
HYUNJOONSHIN <casillaslux@gmail.com>
2023-06-01 17:34:06 +0900

    feat. 1) 게시글 수정 기능 구현 2) 게시글 삭제 기능 구현 중

--- src/modules/BoardPostModule.js
+++ src/modules/BoardPostModule.js
@@ -8,12 +8,16 @@ const GET_BOARDPOSTS = 'boardPost/GET_BOARDPOSTS';
 const GET_BOARDPOST = 'boardPost/GET_BOARDPOST'
 const POST_BOARDPOST = 'boardPost/POST_BOARDPOST';
 const PUT_BOARDPOST = 'boardPost/PUT_BOARDPOST';
+const DELETE_BOARDPOST = 'board/DELETE_BOARDPOST';
 
-export const { boardPost: { getBoardposts, getBoardpost, postBoardpost, putBoardPost } } = createActions({
+
+export const { boardPost: { getBoardposts, getBoardpost, postBoardpost, putBoardpost, deleteBoardpost } } = createActions({
     [GET_BOARDPOSTS]: res => res.data,
     [GET_BOARDPOST]: res => res.data,
     [POST_BOARDPOST]: res => res,
-  [PUT_BOARDPOST]: res => res
+  [PUT_BOARDPOST]: res => res,
+  [DELETE_BOARDPOST]: res => res,
+
 });
 
 /* 리듀서 */
@@ -23,6 +27,7 @@ const boardPostReducer = handleActions(
         [GET_BOARDPOST]: (state, { payload }) => ({ detail: payload }),
         [POST_BOARDPOST]: (state, { payload }) => ({ regist: payload }),
         [PUT_BOARDPOST]: (state, { payload }) => ({ modify: payload }),
+        [DELETE_BOARDPOST]: (state, { payload }) => ({ del: payload }),
     }
     , initialState);
 
