319f6d238664aff27584d4c10de00b83aa625da9
dldbaud <dldbaud20@gmail.com>
2023-06-01 21:09:23 +0900

    docs : 근무 시간 수정

--- src/modules/WorkModule.js
+++ src/modules/WorkModule.js
@@ -7,11 +7,13 @@ const initalState = [];
 const GET_WORKLIST = 'work/GET_WORKLIST';
 const POST_INSERTSTART = 'work/POST_INSERTSTART';
 const PUT_QUITTIME = 'work/PUT_QUITTIME';
+const GET_WORKDAY = 'work/GET_WORKDAY';
 
-export const { work : { getWorklist, postInsertstart, putQuittime }} = createActions({
+export const { work : { getWorklist, postInsertstart, putQuittime, getWorkday }} = createActions({
     [GET_WORKLIST] : (res) => res.data,
     [POST_INSERTSTART] : res => res,
     [PUT_QUITTIME] : res => res,
+    [GET_WORKDAY] : res => res,
 });
 
 const workReducer = handleActions(
@@ -19,6 +21,7 @@ const workReducer = handleActions(
         [GET_WORKLIST] : (state, { payload }) => ({ myWork : payload }),
         [POST_INSERTSTART] : (state, { payload }) => ({ insert : payload}),
         [PUT_QUITTIME] : (state, { payload }) => ({ quit : payload }),
+        [GET_WORKDAY] : (state, { payload}) => ({ wDay : payload }),
     }
 , initalState)
 
