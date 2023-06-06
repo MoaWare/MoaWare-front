fd23e03dce652e58d48e74b397081ee9467b95f8
dldbaud <126305652+dldbaud@users.noreply.github.com>
2023-06-06 14:50:58 +0900

    Update BoardPostRegist.js

--- src/pages/board/BoardPostRegist.js
+++ src/pages/board/BoardPostRegist.js
@@ -20,7 +20,11 @@ function BoardPostRegist() {
     useEffect(
         () => {
             if (regist?.status === 200) {
-                alert('게시글 등록이 완료 되었습니다.');
+                toast.success('게시글 등록이 완료 되었습니다.', {
+                  position: toast.POSITION.TOP_CENTER, // 토스트 위치 (옵션)
+                  autoClose: 2000, // 자동으로 닫히는 시간 (ms) (옵션)
+                  hideProgressBar: false, // 진행 막대 숨김 여부 (옵션)
+                });
                 navigate('/boardPosts', { replace: true });
             }
         },
